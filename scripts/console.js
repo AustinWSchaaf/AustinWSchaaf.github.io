35d23e95d5c7697a0d1ca9ed6340757cb660448b
Austin Schaaf <austin.schaaf@gmail.com>
2020-11-12 11:45:29 -0600

    unfinished

---
+++ scripts/console.js
@@ -0,0 +1,15 @@
+// var terminal = document.getElementById('console-container');
+// terminal.addEventListener('mousedown', function(){ drag(event) });
+
+// var windowheight = window.innerHeight;
+// var windowWidth = window.innerWidth;
+
+
+// var userName =
+//   '<p><span id="a">Skynet</span>:<span id="b">-</span><span id="c">$</span></p>\n';
+
+var user = '<span id="terminal__prompt--user">synet:</span><span id="terminal__prompt--location">~</span><span id="terminal__prompt--bling">$</span>';
+function tfcFunction(event){
+  var x = event.which || event.keyCode;
+  console.log(x);
+}
